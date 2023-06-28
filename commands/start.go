a9ef7004a63b500ef2cca7993d5cb740a1446cef
Denis Smirnov <w1png@yandex.ru>
2023-06-28 22:07:13 +0300

    Add start command

--- commands/start.go
+++ commands/start.go
@@ -6,14 +6,19 @@ import (
 	"github.com/w1png/paid-access-telegram-bot/models"
 )
 
-func StartCommand(msg *tg.Message, update tg.Update, user models.User) (tg.MessageConfig, error) {
-  text, err := language.CurrentLanguage.Get(language.Start)
-  if err != nil {
-    return tg.MessageConfig{}, err
-  }
+func StartCommand(msg *tg.Message, update tg.Update, user *models.User) (tg.MessageConfig, error) {
+  text := language.CurrentLanguage.Get(language.Start)
 
   replyMsg := tg.NewMessage(update.Message.Chat.ID, text)
   replyMsg.ReplyToMessageID = update.Message.MessageID
+
+  if user.IsAdmin() {
+    replyMsg.ReplyMarkup = tg.NewReplyKeyboard(
+      tg.NewKeyboardButtonRow(
+        tg.NewKeyboardButton(language.CurrentLanguage.Get(language.AdminMenu)),
+      ),
+    )
+  }
 
   return replyMsg, nil
 }
